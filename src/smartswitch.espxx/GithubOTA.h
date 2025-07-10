c1b1eb2952bcd8815a2d3d4ade17f4b6694b1782
mlauke <marko.lauke@gmail.com>
2025-07-10 12:15:15 +0200

    +fix: build esp8266 binary

--- src/smartswitch.espxx/GithubOTA.h
+++ src/smartswitch.espxx/GithubOTA.h
@@ -3,29 +3,33 @@
 
 #include "RestClient.h"
 
-#define UPDATE_HOST     "https://api.github.com"
-#define UPDATE_URL      "/repos/mlauke/smartswitch/releases/latest"
-#define UPDATE_TYPE     "application/gzip"
-#define UPDATE_FILENAME "smartswitch.esp8266.ino.bin.gz"
+#define UPDATE_HOST "https://api.github.com"
+#define UPDATE_URL "/repos/mlauke/smartswitch/releases/latest"
+#define UPDATE_TYPE "application/gzip"
 
-class GithubOTA
-{
+#ifdef ESP32
+#define UPDATE_FILENAME "smartswitch.esp32.ino.bin.gz"
+#elif defined(ESP8266)
+#define UPDATE_FILENAME "smartswitch.esp8266.ino.bin.gz"
+#endif
+
+class GithubOTA {
 public:
   String release_tag;
 
-  GithubOTA(const char * , const char * , const char * , const char * );
+  GithubOTA(const char *, const char *, const char *, const char *);
   bool checkUpdate(const char *);
   bool doUpdate(void (*fnOTABegin)(void));
   String getUpdateError();
 
 protected:
-  const char * update_type;
-  const char * update_filename;
-  const char * update_host;
-  const char * update_url;
+  const char *update_type;
+  const char *update_filename;
+  const char *update_host;
+  const char *update_url;
 
   String download_url;
 };
 
 
-#endif //GITHUBOTA_H
+#endif  //GITHUBOTA_H
