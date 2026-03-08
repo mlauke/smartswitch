ed98e0b3fdc3788a2890f2f0169d7dc4567fd16c
mlauke <marko.lauke@gmail.com>
2026-03-08 21:45:07 +0100

    +fix: 20min hysteresis

--- smartswitch.espxx/include/Logic.h
+++ smartswitch.espxx/include/Logic.h
@@ -92,7 +92,7 @@ static BatteryState predictBatteryCapacityState(SystemConfig *systemConfig, Syst
     return BatteryState::Min; // no solar forecast data, assume battery will become empty
   }
 
-  uint16_t hysteresis_Wh = systemConfig->loadPower_W * 15 / 60; // required capacity (Wh) if load is switched on for 15min
+  uint16_t hysteresis_Wh = systemConfig->loadPower_W * 20 / 60; // required capacity (Wh) if load is switched on for 20min
   uint32_t cap_bat_sim_wh = systemState->cap_bat_Wh;
   uint32_t cap_bat_sim_previos_wh;
   uint16_t cap_bat_min_Wh = systemConfig->cap_bat_min_Wh + (systemState->switchEnabled ? 0 : hysteresis_Wh);
